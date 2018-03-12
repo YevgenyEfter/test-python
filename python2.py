8eedad0b8759393008848bb89f7f75cda68f9489
Yevgeny Efter <Yevgeny.Efter@hpe.com>
2018-03-12 09:03:41 +0200

    Adding 2 files of examples

---
+++ python2.py
@@ -0,0 +1,19 @@
+num1 = 2
+print(num1)
+
+num2 = 7.0
+print(num2)
+
+num3 = float(8)
+print(num3)
+
+str1 = "Hello!"
+print (str1)
+
+str2 = 'Hello!'
+print (str2)
+
+a, b = 3, 4
+print(a, b)
+
+print(num1 + str1)
---
+++ python3.py
@@ -0,0 +1,25 @@
+if 0:
+	print("Yes")
+print("Ok")
+
+name = input("Enter your name")
+if name == "a":
+	print("a")
+elif name == "b":
+	print("b")
+else:
+	print("other")
+
+num1 = 1;
+num2 = 2;
+
+if num1 is num1:
+	print("The same instance")
+else:
+	print("Different instances")
+
+b1 = False
+if not b1:
+	print("b1 is false")
+
+print((not False) == False)
