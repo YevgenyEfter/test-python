0971224db5458b47c7c9e54184d015a20b89d249
Yevgeny Efter <Yevgeny.Efter@hpe.com>
2018-03-18 08:58:36 +0200

    Adding a file with slicing.

---
+++ lists2.py
@@ -0,0 +1,20 @@
+l = [34, "ab", 56, 12.12]
+print(l[-1])
+
+a = l[:]
+print(a)
+
+b = l[2:]
+print(b)
+
+c = l[:2]
+print(c)
+
+d = l[::2]
+print(d)
+
+e = l[::-1]
+print(e)
+
+l2 = l[1:2:1]
+print(l2)
