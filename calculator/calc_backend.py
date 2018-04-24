f7a167bd4cdd185bbfe0b6e74b686a1c06566828
Yevgeny Efter <Yevgeny.Efter@hpe.com>
2018-04-24 10:21:22 +0300

    Adding a command line interface for the calculator.

---
+++ calculator/calc_backend.py
@@ -0,0 +1,18 @@
+class CalculatorBackend:
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
+
+    def integerDivide(self, a, b):
+        return a // b
---
+++ calculator/calc_cli_client.py
@@ -0,0 +1,47 @@
+from calc_backend import CalculatorBackend
+
+EXIT_CODE = 9
+
+calculator = CalculatorBackend()
+
+print("Welcome to the Calculator!\n")
+
+userRequest = 0
+while userRequest != EXIT_CODE:
+    print("Please choose an operation:")
+    print("1. Add numbers.")
+    print("2. Subtract numbers.")
+    print("3. Multiply numbers.")
+    print("4. Divide numbers.")
+    print("5. Divide integer numbers.\n")
+    print(str(EXIT_CODE) + ". Exit.")
+
+    userRequest = int(input())
+
+    if userRequest == EXIT_CODE:
+        break
+
+    a = int(input("Please enter the 1st number: "))
+    b = int(input("Please enter the 2nd number: "))
+
+    result = -1
+
+    if userRequest == 1:
+        result = calculator.add(a, b)
+    elif userRequest == 2:
+        result = calculator.subtract(a, b)
+    elif userRequest == 3:
+        result = calculator.multiply(a, b)
+    elif userRequest == 4:
+        try:
+            result = calculator.divide(a, b)
+        except ZeroDivisionError:
+            print("Can't divide by 0. Try again.")
+            continue
+    elif userRequest == 5:
+        result = calculator.integerDivide(a, b)
+
+    print("The result is " + str(result))
+
+
+print("Thanks for using the Calculator!")
